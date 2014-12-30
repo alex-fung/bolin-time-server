d92c9e2634dfc823058335a15404bccb4357333e
alex-fung <alexcfung@gmail.com>
2014-12-29 22:24:31 -0500

    fixed api

--- api.py
+++ api.py
@@ -1,21 +1,15 @@
-from flask import Flask
-from flask.ext.restful import reqparse, Api, Resource
+import json
+
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
-api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('id', type=str)
-
-class UploadVideoLink(Resource):
-	def post(self):
-		args = parser.parse_args()
-		id = {'id': args['id']}
-		return id, 201
-		
-api.add_resource(UploadVideoLink, '/')
+@app.route('/', methods=['POST'])
+def test():
+	res = json.loads(request.data)
+	return jsonify(**{'id': res['id']})
 
 if __name__=='__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(debug=True, port=port)
+	app.run(host='0.0.0.0', debug=True, port=port)
