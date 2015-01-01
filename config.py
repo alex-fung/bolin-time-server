605a24d568beebbfd885ba639ce8f1680b0e9728
alex-fung <alexcfung@gmail.com>
2014-12-31 22:26:06 -0500

    changes to config

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config(object):
 	TESTING = False
 	CSRF_ENABLED = True
 	SECRET_KEY = 'Northern-air-temple'
-	DATABASE_URL="postgresql://bolin:opalandbolin@bolintimeinstance.c9hc8vdkzflp.us-west-2.rds.amazonaws.com:5432/bolintimedatabase"
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
